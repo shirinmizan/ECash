40f6204e5eb698b7a3545fe6e7f91dba75508dfe
Shirin Mizan <shirinmizan@gmail.com>
2016-12-03 18:14:17 -0500

    Initial commit

--- ECash/Properties/AssemblyInfo.cs
+++ ECash/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(fals)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("04f48acb-8c2c-4230-824a-603720ea1707")]
