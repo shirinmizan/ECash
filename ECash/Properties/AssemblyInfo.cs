656297d794203b336795db8b60ef94e72fa3ceee
Shirin Mizan <shirinmizan@gmail.com>
2016-12-03 18:10:12 -0500

    fixed the name

--- ECash/Properties/AssemblyInfo.cs
+++ ECash/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("E-ATM")]
+[assembly: AssemblyTitle("ECash")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("E-ATM")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("ECash")]
+[assembly: AssemblyCopyright("Copyright ©  2016)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
